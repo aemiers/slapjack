e4755cce99ba67bd1fc4416a8a783692e5a5fa5f
Anneke Miers <annekemiers.am@gmail.com>
2021-01-12 15:48:19 -0700

    Create base structure for game class

---
+++ game.js
@@ -0,0 +1,39 @@
+class Game {
+  constructor() {
+    this.currentPlayer = player1;
+  }
+
+  dealCards() {
+
+  }
+
+  playerTurn() {
+
+  }
+
+  addToCenterPile() {
+
+  }
+
+  slap() {
+
+  }
+
+  shuffleDeck() {
+
+  }
+
+  updateWins() {
+
+  }
+
+  resetDeck() {
+// newgame
+  }
+
+}
+
+var player1 = new Player();
+var player2 = new Player();
+var allCards = []
+var centerPile = [];
